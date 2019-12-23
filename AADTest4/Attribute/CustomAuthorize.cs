9a11b99a73568ff0157f125a99fd18568fa1d7da
Simon Wu (APEX) <siw@microsoft.com>
2019-12-23 17:27:49 +0800

    add dotnet framework version

---
+++ AADTest4/Attribute/CustomAuthorize.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AADTest4.Attribute
+{
+    public class CustomAuthorize : AuthorizeAttribute
+    {
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            throw new Exception("Authorization fails.");
+        }
+    }
+}
